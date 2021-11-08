9298524947b6c4dd57386136c2a680be337be929
Jubayer <jubayer7a@gmail.com>
2021-11-08 10:35:37 -0500

    Task A completed

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
-#include "funcs.h"
+#include <cmath>
 
+#include "funcs.h"
+#include "coord3d.h"
 // add functions here
 
+double length(Coord3D *p) {
+	double x_val = p->x;
+	double y_val = p->y;
+	double z_val = p->z;
+
+	double distance = sqrt((x_val*x_val) + (y_val*y_val) + (z_val*z_val));
+
+	return distance;
+}
+
+// int main(int argc, char const *argv[])
+// {
+// 	Coord3D pointP = {10, 20, 30};
+// 	std::cout << length(&pointP) << '\n';
+	
+// 	return 0;
+// }
