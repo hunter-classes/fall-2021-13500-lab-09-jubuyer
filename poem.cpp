43751dc1b773486b882f5f469f6966ca4675699c
Jubayer <jubayer7a@gmail.com>
2021-11-08 13:14:30 -0500

    Task D completed

--- poem.cpp
+++ poem.cpp
@@ -8,11 +8,19 @@ string * createAPoemDynamically() {
 }
 
 int main() {
+    int i = 0;
+
     while(true) {
         string *p;
         p = createAPoemDynamically();
 
-        // assume that the poem p is not needed at this point
+        delete p;
+        i++;
+        if (i == 3) { //so that memory isn't constantly used
+            break; //don't need to use ctrl+c to stop code
+        }
 
     }
+
+    return 0;
 }
