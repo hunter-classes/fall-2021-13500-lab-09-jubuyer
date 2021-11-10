b4211c030738d10f9e8320ddb79378154438f57a
jubuyer <jahmed10@stuy.edu>
2021-11-10 13:32:36 -0500

    Framework for testing tasks

--- tests.cpp
+++ tests.cpp
@@ -3,3 +3,18 @@
 #include "funcs.h"
 
 // add your tests here
+TEST_CASE("Test for length [TASK A]") {
+
+}
+
+TEST_CASE("Test for fartherFromOrigin [TASK B]") {
+
+}
+
+TEST_CASE("Tests for move [TASK C]") {
+
+}
+
+TEST_CASE("Tests for createCoord3D/deleteCoord3D [TASK E]") {
+  
+}
